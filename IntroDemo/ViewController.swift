d298945421cb324ce5965846a4dc24c07c2b76f4
Thành Lã <huuthanhla@gmail.com>
2019-02-15 08:46:30 +0700

    Init project

--- IntroDemo/ViewController.swift
+++ IntroDemo/ViewController.swift
@@ -7,14 +7,91 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    var scrollView: UIScrollView!
+    var pageControl: UIPageControl!
+    
+    
+    var timer: Timer?
+    var currentIndex: Int = 0
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupViews()
+        
+        images = (1...10).map { _ -> UIImageView in
+            let imageView = UIImageView()
+            imageView.backgroundColor = UIColor.randomDark
+            return imageView
+        }
+        
+        pageControl.numberOfPages = images.count
+        setupSlideScrollView(slides: images)
     }
 
+    fileprivate func setupViews() {
+        scrollView = setupScrollView()
+        scrollView.delegate = self
+        view.addSubview(scrollView)
+        
+        pageControl = setupPageControl()
+        view.addSubview(pageControl)
+        
+        setupConstrants()
+    }
+    
+    fileprivate func setupSlideScrollView(slides : [UIImageView]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+    }
+    
+    fileprivate func setupScrollView() -> UIScrollView {
+        let scrollView = UIScrollView()
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        return scrollView
+    }
+    
+    fileprivate func setupPageControl() -> UIPageControl {
+        let pageControl = UIPageControl()
+        pageControl.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        return pageControl
+    }
 
+    fileprivate func setupConstrants() {
+        scrollView.snp.makeConstraints { (make) in
+            make.edges.equalTo(view)
+        }
+        
+        pageControl.snp.makeConstraints { (make) in
+            make.bottom.equalTo(view.snp.bottom).inset(10)
+            make.centerX.equalTo(view.snp.centerX)
+        }
+    }
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x / view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+    }
+}
+
+extension UIColor {
+    static var randomDark: UIColor {
+        let hue = CGFloat.random(in: 0...1)
+        let saturation = CGFloat.random(in: 0...1)
+        let brightness = CGFloat.random(in: 0.4...0.8)
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1.0)
+    }
+}
